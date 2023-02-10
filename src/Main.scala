b0cabcc2036d6c0fe94a7042fc3d6d6e59d6d4c5
Cebula <patyczak.pat@vp.pl>
2023-02-10 18:55:33 +0100

    Ready to play

--- src/Main.scala
+++ src/Main.scala
@@ -9,7 +9,7 @@ object Main extends App{
   val frontend = new Frontend()
 
   class Backend{
-    val board : Set[Int] = ((1 to 10).map( x => Random.between(0,64))).toSet
+    val board : Set[Int] = ((1 to 10).map( x => Random.between(1,65))).toSet
 
     println(board)
   }
@@ -18,25 +18,55 @@ object Main extends App{
     size = new Dimension(640, 640)
     title = "SAPER"
     visible = true
+    val points = new Label("0")
+
     contents = new BoxPanel(Orientation.Vertical){
+      contents += new BoxPanel(Orientation.Horizontal){
+        contents += new NewGameButton()
+      }
       (1 to 8).map(x =>
-        contents += new BoxPanel(Orientation.Horizontal){
-          (1 to 8).map(y => contents += new myButton((x-1)*8 + y) )
+        contents +=  new BoxPanel(Orientation.Horizontal){
+          (1 to 8).map(y => contents += new saperField((x-1)*8 + y) )
           }
         )
+      contents += new BoxPanel(Orientation.Horizontal){
+        contents += points
+        }
       }
 
-
-    class myButton (val position : Int ) extends Button{
+    class saperField (val position : Int ) extends Button{
       minimumSize = new Dimension(80,80)
       preferredSize = new Dimension(80,80)
       maximumSize = new Dimension(80,80)
-      action = new Action(position.toString) {
+      lazy val fieldValue = Random.between(0,5)
+      action = new Action("") {
         def apply() =
-          if backend.board.contains(position) then println("Buum")
-          else println("Point")
+          if backend.board.contains(position) then
+            action = new Action("Bum") {
+              def apply() = {
+                Dialog.showMessage(Label.apply(), s"You got ${points.text} score", "Game Over")
+                NewGameButton.apply().newGame()
+              }
+            }
+            action.apply()
+          else
+            action = new Action(fieldValue.toString) {
+              def apply() = {}
+            }
+            points.text = (points.text.toInt + fieldValue).toString
       }
 
+      }
+    }
+    class NewGameButton extends Button{
+      def newGame() ={
+        val newContent = new Frontend()
+        newContent.visible = false
+        frontend.contents = newContent.contents.apply(0)
+        newContent.dispose()
+      }
+      action = new Action("New Game"){
+        def apply() = newGame()
+      }
     }
   }
-}
